874898c6cc12eda02cf1fae85dfac653a3c9312b
Alexey Tarasov <tarasovion2004@gmail.com>
2022-11-27 11:11:13 +0300

    Delete invalid session cookies

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,6 @@
 import { userFromToken } from "$lib/auth";
 import { prisma } from "$lib/db";
+import { serialize } from "cookie";
 
 export const handle = async ({ event, resolve }) => {
   let settings = await prisma.settings.findFirst({
@@ -19,7 +20,7 @@ export const handle = async ({ event, resolve }) => {
 
   const response = await resolve(event);
   if (!event.locals.user) {
-    event.cookies.delete("session");
+    response.headers.set("set-cookie", serialize("session", "", { expires: new Date(Date.now() - 3600) }));
   }
 
   return response;
